63dda88ab85449f5dc9760f40ad92b9aa415982f
acamilaofc <camikacs@gmail.com>
2022-04-27 15:14:39 -0300

    Aula FIAP 2704

---
+++ assets/js/todo.js
@@ -0,0 +1,9 @@
+const NAME = "Camila C Santos";
+let email = "camikacs@gmail.com";
+
+function escrever(name, email) {
+  console.log(`Nome: ${name}`);
+  console.log(`Email: ${email}`);
+}
+
+escrever(NAME, email);
