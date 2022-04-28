61d4bd70cb7d73cdbae37882c131ba48e0f2c604
acamilaofc <camikacs@gmail.com>
2022-04-28 15:37:45 -0300

    Aula FIAP 2804

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,9 +1,51 @@
-const NAME = "Camila C Santos";
-let email = "camikacs@gmail.com";
+const db = [
+  {
+    id: 1,
+    title: "Concluir App Conexão Arte",
+    steps: [
+      {
+        step: "Ajustar textos",
+      },
+      {
+        step: "Trocar imagens para imagens públicas",
+      },
+      {
+        step: "Publicar no Expo",
+      },
+      {
+        step: "Publicar no Expo Store Connect",
+      },
+    ],
+    done: false,
+    dueDate: "2022-05-06",
+    reminder: "2022-05-02 10:00",
+  },
+  {
+    id: 2,
+    title: "Aula 4 Fiap - Avanade",
+    steps: [{ step: "Atributos Globais" }, { step: "Estruturas CSS" }],
+    done: true,
+    dueDate: "2022-04-28",
+  },
+];
 
-function escrever(name, email) {
-  console.log(`Nome: ${name}`);
-  console.log(`Email: ${email}`);
+// console.log(db[0].title);
+
+const newTask = document.querySelector("#inputTxtNewTask");
+const form = document.querySelector("#addNewTask");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+newTask.addEventListener("keyup", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  if (e.key == "Enter") {
+    alert(newTask.value);
+    db.push({ id: Number(db.length) + 1, title: newTask.value });
+    newTask.value = "";
+    console.log(db);
+  }
+});
+{
 }
-
-escrever(NAME, email);
---
+++ assets/js/utils/domFuncions.js
@@ -0,0 +1,16 @@
+export function addTask() {
+  alert("cadastrando uma nova tarefa");
+}
+
+export function removeTask() {
+  alert("Removendo uma nova tarefa");
+}
+export function updateTask() {
+  alert("Alterando uma nova tarefa");
+}
+export function getAllTask() {
+  alert("Listando todas tarefas");
+}
+export function getTaskById() {
+  alert("Exibindo uma tarefa");
+}
